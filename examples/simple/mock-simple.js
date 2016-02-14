d9325f4d8a2916d01142bdb98d4f118698c0f824
Denys Feshchenko <denis.feshchenko@gmail.com>
2016-02-15 00:17:44 +0200

    defined expected new api

--- examples/simple/mock-simple.js
+++ examples/simple/mock-simple.js
@@ -1,26 +1,26 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-var mockBackend = require('../../lib/mock-backend');
+var bmock = require('../../index.js');
 var path = require('path');
-
-var mock = mockBackend.mock;
-var g = mockBackend.getters;
-var r = mockBackend.rules;
-var m = mockBackend.matchers;
 
 var app = express();
 
 app.use(bodyParser.json());
 
-mock.config({
+bmock.config({
   responseDir: path.join(__dirname, './data')
 });
 
 var rules = {
-  deposit: r.mark('error', g.queryProp('amount'), m.equal('200'))
+  deposit: bmock.mark('error').if.queryProp('amount').equal('200'),
+  purchase: bmock.mark().by.queryProp('type'),
+  registration: [
+    bmock.mark('error').if.queryProp('name').equal('invalid'),
+    bmock.mark('unfinished').if.queryProp('hobby').equal('none')
+  ]
 };
 
-app.use('/api/v1/purchase', mock.response('purchase', r.mark('bonus', g.queryProp('count'), m.equal('2'))));
-app.use('/api/v2/:command*', mock.response(g.param('command'), rules));
+app.use('/api/v2/:command*', bmock.response(bmock.getters.param('command'), rules));
+app.use('/api/v1/validate', bmock.response('validate', bmock.mark('error').if.queryProp('valid').equal('false')));
 
 app.listen(5000);
